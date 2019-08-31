93f9763fd92c75ffe448618d5d405097fec5bcf9
NicHub <nicolas.jeanmonod@gmail.com>
2019-08-31 18:01:38 +0200

    Serial USB (USB CDC)

---
+++ src/System_Clock_Configuration.h
@@ -0,0 +1,57 @@
+/*
+ * S T M 3 2 - E 4 0 7    B L I N K
+ *
+ * Copyright (C) 2019  Nicolas Jeanmonod, ouilogique.com
+ *
+ *
+ * The function `SystemClock_Config(void)` overides the default one located in
+ * ~/.platformio/packages/framework-arduinoststm32/variants/BLACK_F407XX/variant.cpp
+ * This is needed for SerialUSB to work.
+ * See https://community.platformio.org/t/stm32-e407-doesn-t-work-on-pio/9303/3
+ *
+ *
+ */
+
+/**
+  * @brief  System Clock Configuration
+  * @param  None
+  * @retval None
+  *
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  */
 
 #include <Arduino.h>
-
+#include <System_Clock_Configuration.h> // Needed for SerialUSB to work.
 #define LED_STME407 PC13 // Built-in LED. Inverted logic.
 #define LED_ARDUINO PA5  // Labeled D13 on the board.
 
@@ -18,15 +18,19 @@ void setup()
     pinMode(LED_STME407, OUTPUT);
     pinMode(LED_ARDUINO, OUTPUT);
 
+    // Serial USB (USB CDC) is read through USB-OTG#1 virtual com interface.
+    SerialUSB.begin();
+    Serial3.print("\nSerialUSB START");
+
     // Serial 3 is read with an UART Serial Bridge connected on
     // TX and GND pins of the BOOT header located beside the UEXT connector.
     Serial3.begin(BAUD_RATE); // Change BAUD_RATE in platformio.ini
-    Serial3.print("\nSTART");
+    Serial3.print("\nSerial3 START");
 
     // Serial 6 is read with an UART Serial Bridge connected on
     // pin 3 (TX) and pin 2 (GND) of the UEXT connector.
     Serial6.begin(BAUD_RATE); // Change BAUD_RATE in platformio.ini
-    Serial6.print("\nSTART");
+    Serial6.print("\nSerial6 START");
 }
 
 /**
@@ -34,17 +38,21 @@ void setup()
  */
 void loop()
 {
-    Serial3.print("\n");
+    SerialUSB.print("\nSerialUSB ");
+    SerialUSB.print(millis());
+    Serial3.print("\nSerial3 ");
     Serial3.print(millis());
-    Serial6.print("\n");
+    Serial6.print("\nSerial6 ");
     Serial6.print(millis());
 
+    SerialUSB.print("  LED ON");
     Serial3.print("  LED ON");
     Serial6.print("  LED ON");
     digitalWrite(LED_STME407, LOW);
     digitalWrite(LED_ARDUINO, HIGH);
     delay(20);
 
+    SerialUSB.print("  LED OFF");
     Serial3.print("  LED OFF");
     Serial6.print("  LED OFF");
     digitalWrite(LED_STME407, HIGH);
